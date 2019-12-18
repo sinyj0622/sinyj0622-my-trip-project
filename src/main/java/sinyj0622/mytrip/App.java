415cfa572da03dfb77ece17a031b931acebfb0e4
eunjeong <euuun22@naver.com>
2019-12-18 16:35:17 +0900

    222 출력 삭제

--- src/main/java/sinyj0622/mytrip/App.java
+++ src/main/java/sinyj0622/mytrip/App.java
@@ -9,6 +9,5 @@ public class App {
     public static void main(String[] args) {
         System.out.println("당신의 여행에 함께하겠습니다!");
         System.out.println("111");
-        System.out.println("222");
     }
 }
