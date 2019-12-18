f3791f820570b99b11e9e53ab0fd4cd2484e49ee
eunjeong <euuun22@naver.com>
2019-12-18 16:03:50 +0900

    프 로 젝 트  초 기 화

---
+++ src/main/java/sinyj0622/mytrip/App.java
@@ -0,0 +1,12 @@
+// 프로젝트: 여행 정보 및 일정 관리시스템 
+package sinyj0622.mytrip;
+
+public class App {
+    public String getGreeting() {
+        return "Hello world.";
+    }
+
+    public static void main(String[] args) {
+        System.out.println("당신의 여행에 함께하겠습니다!");
+    }
+}
