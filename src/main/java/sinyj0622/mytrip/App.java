a0fed42e3c50b00d98cac85458c76eeeb267d837
eunjeong <euuun22@naver.com>
2020-02-01 21:26:54 +0900

    Observer 디자인 패턴을 적용

--- src/main/java/sinyj0622/mytrip/App.java
+++ src/main/java/sinyj0622/mytrip/App.java
@@ -1,22 +1,18 @@
 package sinyj0622.mytrip;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.util.ArrayDeque;
-import java.util.ArrayList;
 import java.util.Deque;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Queue;
 import java.util.Scanner;
+import java.util.Set;
 
+import sinyj0622.mytrip.context.ApplicationContextListener;
 import sinyj0622.mytrip.domain.Board;
 import sinyj0622.mytrip.domain.Member;
 import sinyj0622.mytrip.domain.Plan;
@@ -41,19 +37,45 @@ import sinyj0622.mytrip.util.Prompt;
 
 public class App {
 
-	static Scanner keyboard = new Scanner(System.in);
-	static Deque<String> commandStack = new ArrayDeque<>();
-	static Queue<String> commandQueue = new LinkedList<>();
-	static List<Board> boardList = new ArrayList<>();
-	static List<Member> memberList = new LinkedList<>();
-	static List<Plan> planList = new ArrayList<>();
+	Scanner keyboard = new Scanner(System.in);
+	Deque<String> commandStack = new ArrayDeque<>();
+	Queue<String> commandQueue = new LinkedList<>();
 
-	public static void main(String[] args) {
+	
 
-		loadBoardData();
-		loadMemberData();
-		loadPlanData();
+	Set<ApplicationContextListener> listeners = new HashSet<>();
+	Map<String, Object> context	= new HashMap<>();
 
+	void addApplicationContextListener(ApplicationContextListener listener) {
+		listeners.add(listener);
+	}
+
+	void removeApplicationContextListener(ApplicationContextListener listener) {
+		listeners.remove(listener);
+	}
+
+	void notifyContextInitialized() {
+		for (ApplicationContextListener listener : listeners) {
+			listener.ContextInitialized(context);
+		}
+	}
+
+	void notifyContextDestroyed() {
+		for (ApplicationContextListener listener : listeners) {
+			listener.ContextDestroyed(context);
+		}
+	}
+
+
+	@SuppressWarnings("unchecked")
+	void service() {
+
+		notifyContextInitialized();
+
+		List<Board> boardList = (List<Board>) context.get("boardList");
+		List<Member> memberList = (List<Member>) context.get("memberList");
+		List<Plan> planList = (List<Plan>) context.get("planList");
+	
 		Prompt prompt = new Prompt(keyboard);
 		HashMap<String, Command> commandMap = new HashMap<>();
 
@@ -117,15 +139,14 @@ public class App {
 
 		keyboard.close();
 
-		saveBoardData();
-		saveMemberData();
-		savePlanData();
+
+		notifyContextDestroyed();
 
 	}
 
 
 
-	public static void printCommandHistory(Iterator<String> iterator) {
+	public void printCommandHistory(Iterator<String> iterator) {
 		int count = 0;
 		while (iterator.hasNext()) {
 			System.out.println(iterator.next());
@@ -142,96 +163,15 @@ public class App {
 	}
 
 
-	public static void loadBoardData() {
+	
 
-		try(ObjectInputStream in = new ObjectInputStream(
-				new BufferedInputStream(new FileInputStream("./board.ser2")))) {
+	public static void main(String[] args) {
 
-			boardList =  (List<Board>) in.readObject();
-			
-			System.out.printf("총 %d개의 게시글 데이터를 로딩했습니다.\n", boardList.size());
-			
-			} catch (Exception e) {
-				System.out.println("파일 읽기 중 오류 발생! - " + e.getMessage());
-			}  
-
-		}
-
-
-		public static void saveBoardData() {
-			
-			try (ObjectOutputStream out = new ObjectOutputStream(
-					new BufferedOutputStream(new FileOutputStream("./board.ser2")))) {
-				
-				out.writeObject(boardList);
-			
-					
-				System.out.printf("총 %d개의 게시글을 저장하였습니다.\n", boardList.size());
-
-			} catch (IOException e) {
-				System.out.println("파일 쓰기 중 오류 발생! - " + e.getMessage());
-
-			}
-		}
-
-		public static void loadMemberData() {
-
-			try(ObjectInputStream in = new ObjectInputStream(
-					new BufferedInputStream(new FileInputStream("./member.ser2")))) {
-
-				memberList = (List<Member>) in.readObject();
-				
-				System.out.printf("총 %d개의 회원 데이터를 로딩했습니다.\n", memberList.size() );
-				
-			} catch (Exception e) {
-				System.out.println("파일 읽기 중 오류 발생! - " + e.getMessage());
-			} 
-			
-		}
-
-		public static void saveMemberData() {
-
-			try (ObjectOutputStream out = new ObjectOutputStream(
-					new BufferedOutputStream(new FileOutputStream("./member.ser2")))){
-				
-				out.writeObject(memberList);
-				
-				System.out.printf("총 %d개의 회원 데이터를 저장하였습니다.\n", memberList.size() );
-
-
-			} catch (IOException e) {
-				System.out.println("파일 저장 중 오류 발생! - " + e.getMessage());
-			} 
-		}
-
-		public static void loadPlanData() {
-
-			try (ObjectInputStream in = new ObjectInputStream(
-					new BufferedInputStream(new FileInputStream("./plan.ser2")))) {
-				
-				planList = (List<Plan>) in.readObject();
-				
-				System.out.printf("총 %d개의 여행계획 데이터를 로딩했습니다.\n", planList.size());
-
-
-			} catch (Exception e) {
-				System.out.println("파일 읽기 중 오류 발생! - " + e.getMessage());
-			}  
-
-		}
-
-		public static void savePlanData() {
-
-			try (ObjectOutputStream out = new ObjectOutputStream(
-					new BufferedOutputStream(new FileOutputStream("./plan.ser2")))) {
-
-				out.writeObject(planList);
-				
-				System.out.printf("총 %d개의 여행계획 데이터를 저장하였습니다.\n", planList.size());
-
-
-			} catch (IOException e) {
-				System.out.println("파일 저장 중 오류 발생! - " + e.getMessage());
-			} 
-		}
+		App app = new App();
+		
+		app.addApplicationContextListener(new DataLoaderListener());
+		
+		
+		app.service();
 	}
+}
